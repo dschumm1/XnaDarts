41647fbefff03fc0ceaa2d074870ddd5c61eb4bd
Martin Persson <git@martinpersson.org>
2015-03-14 04:48:45 +0100

    Refactoring.

--- XnaDarts/XnaDarts/XnaDarts/Screens/GameModeScreens/Components/CricketMarksComponent.cs
+++ XnaDarts/XnaDarts/XnaDarts/Screens/GameModeScreens/Components/CricketMarksComponent.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using XnaDarts.Gameplay;
 using XnaDarts.Gameplay.Modes;
 using XnaDarts.ScreenManagement;
 
@@ -9,9 +10,12 @@ namespace XnaDarts.Screens.GameModeScreens.Components
 {
     public class CricketMarksComponent : IDrawableGameComponent
     {
-        private const float Scale = 0.8f;
+        private const float Scale = 0.66f;
         private Texture2D _bullTexture;
         private Texture2D _closedTexture;
+        private Vector2 _closedTextureSize;
+        private Vector2 _markTextureSize;
+        private Vector2 _numberTextureSize;
         private readonly Texture2D[] _markTexture = new Texture2D[4];
         private readonly Cricket _mode;
         private readonly Texture2D[] _numberTextures = new Texture2D[6];
@@ -28,118 +32,119 @@ namespace XnaDarts.Screens.GameModeScreens.Components
                 _markTexture[i] =
                     content.Load<Texture2D>(@"Images\" + XnaDartsGame.Options.Theme + @"\" + @"Marks\Mark" + i);
             }
+            _markTextureSize = new Vector2(_markTexture[0].Width, _markTexture[0].Height);
 
             for (var i = 0; i < 6; i++)
             {
                 _numberTextures[i] =
-                    content.Load<Texture2D>(@"Images\" + XnaDartsGame.Options.Theme + @"\" + @"CricketNumbers\" + (i + 15));
+                    content.Load<Texture2D>(@"Images\" + XnaDartsGame.Options.Theme + @"\" + @"CricketNumbers\" +
+                                            (i + 15));
             }
 
-            _bullTexture = content.Load<Texture2D>(@"Images\" + XnaDartsGame.Options.Theme + @"\" + @"CricketNumbers\Bull");
+            _numberTextureSize = new Vector2(_numberTextures[0].Width, _numberTextures[0].Height);
+
+            _bullTexture =
+                content.Load<Texture2D>(@"Images\" + XnaDartsGame.Options.Theme + @"\" + @"CricketNumbers\Bull");
             _closedTexture =
                 content.Load<Texture2D>(@"Images\" + XnaDartsGame.Options.Theme + @"\" + @"CricketNumbers\Closed");
+            _closedTextureSize = new Vector2(_closedTexture.Width, _closedTexture.Height);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            var position = new Vector2(XnaDartsGame.Viewport.Width*0.5f, XnaDartsGame.Viewport.Height*0.1f);
-            const float scale = 0.6f;
+            const int defaultNumberOfSegments = 7;
+            var scaling = defaultNumberOfSegments/(float) _mode.Segments.Count*Scale;
 
-            var tempColor = Color.Black;
-            tempColor.A = 80;
+            var marksPadding = _markTextureSize.Y*0.1f;
+            var marksHeight = _markTextureSize.Y + marksPadding;
+            var marksSpacing = marksHeight*scaling + marksPadding;
 
-            //Draw panels for marks
-            spriteBatch.Draw(ScreenManager.BlankTexture,
-                new Rectangle((int) (position.X - _markTexture[0].Width*1.5f*scale),
-                    (int) (position.Y - _markTexture[0].Height*0.5f*scale),
-                    (int) (_markTexture[0].Width*scale),
-                    (int) (_markTexture[0].Height*7*scale)), tempColor);
+            var viewport = new Vector2(XnaDartsGame.Viewport.Width, XnaDartsGame.Viewport.Height);
+            var marksCenter = viewport*new Vector2(0.5f, 0.4f);
+            var marksOffset = marksSpacing*(_mode.Segments.Count - 1)*0.5f;
 
-            spriteBatch.Draw(ScreenManager.BlankTexture,
-                new Rectangle((int) (position.X + _markTexture[0].Width*0.5f*scale),
-                    (int) (position.Y - _markTexture[0].Height*0.5f*scale),
-                    (int) (_markTexture[0].Width*scale),
-                    (int) (_markTexture[0].Height*7*scale)), tempColor);
+            var panelPadding = marksSpacing;
+            var panelWidth = marksSpacing*1.1f;
+            var panelSpacing = panelWidth + panelPadding;
+            var panelHeight = marksSpacing*_mode.Segments.Count + marksSpacing*0.2f;
+            var panelOffset = panelSpacing*(_mode.Players.Count - 1)*0.5f*Vector2.UnitX;
 
+            for (var i = 0; i < _mode.Players.Count; i++)
+            {
+                var player = _mode.Players[i];
+                var panelColor = _mode.GetPlayerColor(player)*0.33f;
+                var panelPosition = marksCenter - panelOffset + panelSpacing*i*Vector2.UnitX;
 
-            //Draw marks
+                var rectangle = new Rectangle(
+                    (int) (panelPosition.X - panelWidth*0.5f),
+                    (int) (panelPosition.Y - panelHeight*0.5f),
+                    (int) panelWidth,
+                    (int) panelHeight
+                    );
+
+                spriteBatch.Draw(ScreenManager.BlankTexture, rectangle, panelColor);
+
+                for (var j = 0; j < _mode.Segments.Count; j++)
+                {
+                    var segment = _mode.Segments[j];
+                    var markPosition = marksCenter - new Vector2(panelOffset.X, marksOffset) +
+                                       new Vector2(panelSpacing * i, marksSpacing * j);
+                    drawMark(spriteBatch, segment, player, markPosition, scaling);
+                }
+            }
+
             for (var i = 0; i < _mode.Segments.Count; i++)
             {
-                drawMarks(spriteBatch, position, _mode.Segments[i]);
-                position.Y += _markTexture[0].Height*scale;
+                var numberPosition = marksCenter - new Vector2(0, marksOffset) + new Vector2(0, marksSpacing)*i;
+                var segment = _mode.Segments[i];
+                drawNumber(spriteBatch, segment, numberPosition);
             }
         }
 
-        private void drawMarks(SpriteBatch spriteBatch, Vector2 position, CricketSegment segment)
+        private void drawNumber(SpriteBatch spriteBatch, CricketSegment segment, Vector2 numberPosition)
         {
-            var font = ScreenManager.Trebuchet24;
-            var temp = position - new Vector2(_markTexture[0].Width*1.5f*Scale, _markTexture[0].Height*0.5f*Scale);
-            Vector2 offset;
-            Color segmentColor;
+            var numberOffset = _numberTextureSize*0.5f;
+            Texture2D numberTexture;
+            var segmentColor = Color.White;
 
-            foreach (var player in _mode.Players)
+            if (!segment.IsOpen)
             {
-                //draw marks
-                var marks = segment.GetScoredMarks(player);
-
-                segmentColor = Color.White;
-
-                if (!segment.IsOpen)
-                {
-                    segmentColor = Color.White*0.33f;
-                }
-
-                if (marks > 0)
-                {
-                    spriteBatch.Draw(_markTexture[Math.Min(marks, 3)], temp, null, segmentColor, 0, Vector2.Zero, Scale,
-                        SpriteEffects.None, 0);
-                }
-
-                var center = new Vector2(_markTexture[0].Width, _markTexture[0].Height)*Scale*0.5f;
-                offset = font.MeasureString(marks.ToString())*0.5f;
-
-                if (XnaDartsGame.Options.Debug)
-                {
-                    spriteBatch.DrawString(font, marks.ToString(), temp + center - offset + Vector2.One,
-                        Color.Black);
-                    spriteBatch.DrawString(font, marks.ToString(), temp + center - offset, Color.White);
-                }
-
-                temp.X += _markTexture[0].Width*Scale*2f;
+                segmentColor *= 0.33f;
             }
 
-            if (XnaDartsGame.Options.Debug)
+            if (segment.Segment == 25)
             {
-                offset = font.MeasureString(segment.ToString())*0.5f;
-                spriteBatch.DrawString(font, segment.ToString(), position - offset + Vector2.One, Color.Black);
-                spriteBatch.DrawString(font, segment.ToString(), position - offset, Color.White);
-            }
-
-            offset = new Vector2(_numberTextures[0].Width, _numberTextures[0].Height)*0.5f;
-            Texture2D tex;
-
-            if (segment.Segment != 25)
-            {
-                tex = _numberTextures[segment.Segment - 15];
+                numberTexture = _bullTexture;
             }
             else
             {
-                tex = _bullTexture;
+                numberTexture = _numberTextures[segment.Segment - 15];
             }
 
-            segmentColor = Color.White;
+            spriteBatch.Draw(numberTexture, numberPosition - numberOffset, segmentColor);
 
             if (!segment.IsOpen)
             {
-                segmentColor = Color.White*0.33f;
+                var closedTextureOffset = _closedTextureSize*0.5f;
+                spriteBatch.Draw(_closedTexture, numberPosition - closedTextureOffset, Color.White);
             }
+        }
 
-            spriteBatch.Draw(tex, position - offset, segmentColor);
+        private void drawMark(SpriteBatch spriteBatch, CricketSegment segment, Player player, Vector2 position,
+            float scaling)
+        {
+            var marks = segment.GetScoredMarks(player);
+            var segmentColor = Color.White;
 
-            if (!segment.IsOpen)
+            if (marks > 0)
             {
-                offset = new Vector2(_closedTexture.Width, _closedTexture.Height)*0.5f;
-                spriteBatch.Draw(_closedTexture, position - offset, Color.White);
+                if (!segment.IsOpen)
+                {
+                    segmentColor *= 0.33f;
+                }
+
+                spriteBatch.Draw(_markTexture[Math.Min(marks, 3)], position, null, segmentColor, 0,
+                    _markTextureSize*0.5f, scaling, SpriteEffects.None, 0);
             }
         }
     }
