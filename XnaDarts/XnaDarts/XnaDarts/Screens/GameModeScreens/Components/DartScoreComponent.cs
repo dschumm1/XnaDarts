7d9e5d6ae238ca478ad48c54cd971e113087a75f
Martin Persson <git@martinpersson.org>
2015-03-14 04:48:58 +0100

    Refactoring.

--- XnaDarts/XnaDarts/XnaDarts/Screens/GameModeScreens/Components/DartScoreComponent.cs
+++ XnaDarts/XnaDarts/XnaDarts/Screens/GameModeScreens/Components/DartScoreComponent.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using XnaDarts.Gameplay;
 using XnaDarts.Gameplay.Modes;
 using XnaDarts.ScreenManagement;
 using XnaDarts.Screens.Menus;
@@ -17,10 +18,13 @@ namespace XnaDarts.Screens.GameModeScreens.Components
         public Texture2D DartTexture;
         private readonly GameMode _mode;
         private readonly Texture2D[] _numberTextures = new Texture2D[3];
+        public bool Vertical;
+        public Vector2 Position;
 
         public DartScoreComponent(GameMode mode)
         {
             _mode = mode;
+            Position = new Vector2(0.5f, 0.7f);
         }
 
         public void LoadContent(ContentManager content)
@@ -41,17 +45,12 @@ namespace XnaDarts.Screens.GameModeScreens.Components
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            DrawDartScore(spriteBatch, false);
-        }
-
-        public virtual void DrawDartScore(SpriteBatch spriteBatch, bool vertical)
-        {
             var viewport = new Vector2(XnaDartsGame.Viewport.Width, XnaDartsGame.Viewport.Height);
-            var centerOfDarts = viewport*new Vector2(0.5f, 0.8f);
+            var centerOfDarts = viewport*Position;
 
             Vector2 orientation;
 
-            if (vertical)
+            if (Vertical)
             {
                 orientation = Vector2.UnitY;
             }
@@ -62,22 +61,17 @@ namespace XnaDarts.Screens.GameModeScreens.Components
 
             var dartsPadding = 0.6f*_dartTextureSize;
             var dartsSpacing = _dartTextureSize + dartsPadding;
-            var dartsOffset = dartsSpacing*(GameMode.DartsPerTurn - 1)*0.5f;
+            var dartsOffset = dartsSpacing*(GameMode.DartsPerTurn - 1)*0.5f * orientation;
 
             //Draw images
             for (var i = 0; i < GameMode.DartsPerTurn; i++)
             {
-                var dartPosition = centerOfDarts + dartsSpacing*i*orientation - dartsOffset;
+                var dartPosition = centerOfDarts - dartsOffset + dartsSpacing * i * orientation;
 
                 if (i < _mode.CurrentPlayerRound.Darts.Count)
                 {
-                    string text;
-                    Color color;
-                    _mode.CurrentPlayerRound.Darts[i].GetVerbose(out text, out color);
-
-                    var textOffset = ScreenManager.Trebuchet32.MeasureString(text)*0.5f;
-                    TextBlock.DrawShadowed(spriteBatch, ScreenManager.Trebuchet32, text, color,
-                        dartPosition - textOffset);
+                    var dart = _mode.CurrentPlayerRound.Darts[i];
+                    drawDartScoreInText(spriteBatch, dart, dartPosition);
                 }
                 else
                 {
@@ -91,6 +85,17 @@ namespace XnaDarts.Screens.GameModeScreens.Components
                     drawNumber(spriteBatch, i, dartPosition);
                 }
             }
+        }
+
+        private static void drawDartScoreInText(SpriteBatch spriteBatch, Dart dart, Vector2 dartPosition)
+        {
+            string text;
+            Color color;
+            dart.GetVerbose(out text, out color);
+
+            var textOffset = ScreenManager.Trebuchet32.MeasureString(text)*0.5f;
+            TextBlock.DrawShadowed(spriteBatch, ScreenManager.Trebuchet32, text, color,
+                dartPosition - textOffset);
         }
 
         private void drawNumber(SpriteBatch spriteBatch, int i, Vector2 position)
