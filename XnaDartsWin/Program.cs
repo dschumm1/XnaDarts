ef27dfbc7d4d8bb4e52c403affdc12e982eecb78
Martin Persson <slask@martinpersson.org>
2016-05-21 17:15:07 +0200

    Removed XnaDartsLinux since the nuget package for MonoGame.Framework.Linux has been removed.

--- XnaDartsWin/Program.cs
+++ XnaDartsWin/Program.cs
@@ -1,19 +1,19 @@
 ﻿using System;
 using XnaDarts;
 
-namespace XnaDartsLinux
+namespace XnaDartsWin
 {
 #if WINDOWS || LINUX
     /// <summary>
-    /// The main class.
+    ///     The main class.
     /// </summary>
     public static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        ///     The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        private static void Main()
         {
             using (var game = new XnaDartsGame())
                 game.Run();
